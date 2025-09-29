ed46fc0929c46e90cec843974ba98e993c4e3dfa
prathamwho <praj79074@gmail.com>
2025-09-30 02:26:54 +0530

    Added getbyId, JWT and verify logic

--- src/index.js
+++ src/index.js
@@ -3,17 +3,32 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
+
+const UserService = require('./services/user-service');
+
 const app = express();
 
 
 const prepareAndStartServer = () => {
-    
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+
     app.use('/api', apiRoutes);
 
     app.listen(PORT, ()=>{
         console.log(`Server Started on Port: ${PORT}`);
+
+        const service = new  UserService();
+
+        // const newToken = service.createToken({email:'hello@admin.com', id:'1'});
+        // console.log("New token is: ", newToken);
+
+        // const token = 'eyJhb...s';
+        // const response = service.verifyToken(token);
+        // console.log(response);
+
+
     })
 }
 
